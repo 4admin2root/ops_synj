18fee6973cef0c173bcdc419250baa10382c5a84
jason <lvzhijun@synjones.com>
2016-11-23 21:54:18 +0800

    add mail func for aide

--- ansible/roles/linux_init_sec/files/aide.sh
+++ ansible/roles/linux_init_sec/files/aide.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mv -f /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz
-/sbin/aide -u > /var/lib/aide/`hostname`_`date +%Y%m%d`.log
+/sbin/aide -u |tee /var/lib/aide/`hostname`_`date +%Y%m%d`.log |mail -s `hostname`_aide_result ops@synjones.net
