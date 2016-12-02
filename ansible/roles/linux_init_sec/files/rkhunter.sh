929e00514010ac4955e295e06645c680c5e6d77d
jason <lvzhijun@synjones.com>
2016-12-02 10:10:33 +0800

    change policy for me

--- ansible/roles/linux_init_sec/files/rkhunter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-/bin/rkhunter --cronjob --quiet --noappend-log
-if [ $? -ne 0 ]
-then
-cat /var/log/rkhunter/rkhunter.log |mail -s "Rkhunter daily run on `uname -n` has produced warning messages" lvzhijun@synjones.net
-fi
