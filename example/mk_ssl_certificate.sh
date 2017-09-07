3209ea5cfae5eda65d81cbe47871d0ba33c0c92b
jason lui <piml.lui@gmail.com>
2017-09-07 17:43:00 +0800

    Create mk_ssl_certificate.sh

---
+++ example/mk_ssl_certificate.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+# create self-signed server certificate:
+
+read -p "Enter your domain [www.example.com]: " DOMAIN
+
+echo "Create server key..."
+
+openssl genrsa -des3 -out $DOMAIN.key 1024
+
+echo "Create server certificate signing request..."
+
+SUBJECT="/C=CN/ST=Beijing/L=Beijing/O=synjones/OU=ctc/CN=$DOMAIN/emailAddress=admin@synjones.net"
+
+openssl req -new -subj $SUBJECT -key $DOMAIN.key -out $DOMAIN.csr
+
+echo "Remove password..."
+
+mv $DOMAIN.key $DOMAIN.origin.key
+openssl rsa -in $DOMAIN.origin.key -out $DOMAIN.key
+
+echo "Sign SSL certificate..."
+
+openssl x509 -req -days 365 -sha256 -in $DOMAIN.csr -signkey $DOMAIN.key -out $DOMAIN.crt
+
+echo "TODO:"
+echo "Copy $DOMAIN.crt to /etc/nginx/ssl/$DOMAIN.crt"
+echo "Copy $DOMAIN.key to /etc/nginx/ssl/$DOMAIN.key"
+echo "Add configuration in nginx:"
+echo "server {"
+echo "    ..."
+echo "    listen 443 ssl;"
+echo "    ssl_certificate     /etc/nginx/ssl/$DOMAIN.crt;"
+echo "    ssl_certificate_key /etc/nginx/ssl/$DOMAIN.key;"
+echo "}"
