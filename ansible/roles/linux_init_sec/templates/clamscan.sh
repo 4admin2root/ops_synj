5ff72fd2c718c9fc42ac91fae206ed577d08bcd0
lvzhijun <lvzhjc@yonyou.com>
2018-09-21 17:21:07 +0800

    fix bug

--- ansible/roles/linux_init_sec/templates/clamscan.sh
+++ ansible/roles/linux_init_sec/templates/clamscan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir /tmp/av
 LOG=/tmp/av/`hostname`_`date +%Y%m%d`_av.log
-clamscan --infected  --recursive /opt /data /data1 >> $LOG
+clamscan --infected  --recursive /usr/lib64 /usr/local/lib /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /root/bin /etc /home /opt /data /data1 >> $LOG
 if [ `grep Infected $LOG |awk '{print $3}'` -gt 0 ]
 then
 cat $LOG | mail -s `hostname`_antivirus_result {{ smtp_send_to }}
